c9293f1de53edc976a80de7998b4f5de470b572b
aradhita2001 <aradhita2001@gmail.com>
2023-08-22 13:39:54 +0530

    video positioning

--- js/background.js
+++ js/background.js
@@ -1,2 +1,22 @@
-let vid = document.getElementById("background_video");
-vid.src = "videos/bg_slow.mp4";
+let video = document.getElementById("background_video");
+let header = document.querySelector("header");
+let fg = document.getElementsByClassName("foreground")[0];
+
+if(window.innerWidth > 768){
+    video.src = "videos/bg_slow.mp4";
+    
+    if(window.innerHeight - video.clientHeight > 25) {
+        console.log("hiiiii");
+        header.style.background = "black";
+        header.style.height = (window.innerHeight - video.clientHeight) + "px";
+        fg.style.paddingTop = header.clientHeight + 30 + "px";
+        video.style.top = header.style.height;
+    }
+}
+
+
+
+
+console.log(video.clientHeight);
+console.log(window.innerHeight);
+console.log(fg);
